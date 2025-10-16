6e416b087ce51d245234284e52b675407132d4dc
cmarti826 <cmarti826@gmail.com>
2025-10-16 12:08:59 -0500

    Create page.tsx

---
+++ app/debug/auth/page.tsx
@@ -0,0 +1,13 @@
+import { createClient } from "@/lib/supabase/server";
+
+export const dynamic = "force-dynamic";
+
+export default async function AuthDebug() {
+  const supabase = createClient();
+  const { data } = await supabase.auth.getUser();
+  return (
+    <pre className="p-4 bg-white border rounded">
+      {JSON.stringify({ user: data.user ?? null }, null, 2)}
+    </pre>
+  );
+}

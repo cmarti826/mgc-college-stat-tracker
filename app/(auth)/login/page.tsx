fd416682e8200e6d34f63f0ad17c7b9c5070710d
cmarti826 <cmarti826@gmail.com>
2025-10-29 14:44:53 -0500

    login fix

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,11 @@
 // app/(auth)/login/page.tsx
+import { Suspense } from 'react'
 import LoginInner from './LoginInner'
 
 export default function LoginPage() {
-  return <LoginInner />
+  return (
+    <Suspense fallback={<div className="flex min-h-screen items-center justify-center bg-white">Loading...</div>}>
+      <LoginInner />
+    </Suspense>
+  )
 }

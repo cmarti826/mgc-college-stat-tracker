d6624d755ee81de4ef6d9a54ec9299fc3667de0e
cmarti826 <cmarti826@gmail.com>
2025-10-29 17:33:15 -0500

    s

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -5,7 +5,4 @@ export const createBrowserSupabase = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      
-    }
   )
